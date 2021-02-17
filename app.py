80302f4be052e761a19db1f838bf22e9de5ef938
Philip Philip <philipandy2@gmail.com>
2021-02-17 09:53:48 +0100

    refractored app

--- app.py
+++ app.py
@@ -1,32 +1,47 @@
 import datetime
 import os
 
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
+from sqlalchemy.exc import IntegrityError
 from forms import SignupForm
 
 from models import Signups
-from database import db_session
+from database import db_session, init_db
 
 app = Flask(__name__)
-app.secret_key = os.environ['APP_SECRET_KEY']
+app.secret_key = os.environ.get('APP_SECRET_KEY', "secret")
 
 
 @app.route("/", methods=('GET', 'POST'))
 def signup():
     form = SignupForm()
     if form.validate_on_submit():
-        signup = Signups(name=form.name.data, email=form.email.data,
+
+        signup = Signups(name=form.name.data,
+                         email=form.email.data,
                          date_signed_up=datetime.datetime.now())
         db_session.add(signup)
-        db_session.commit()
-        return redirect(url_for('success'))
+        try:
+            db_session.commit()
+        except IntegrityError:
+            return "User Already Exists"
+        return redirect(url_for('success', _external=True))
     return render_template('signup.html', form=form)
 
 
 @app.route("/success")
 def success():
-    return "Thank you for signing up!"
+    if request.method == "GET":
+        signups = Signups.query.order_by('id').all()
+        response = []
+        for signup in signups:
+            signup = signup.__dict__
+            del signup["_sa_instance_state"]
+            response.append(signup)
+        print(response)
+        return render_template('success.html', signups=response)
 
 
 if __name__ == '__main__':
+    init_db()
     app.run(host='0.0.0.0', port=5090)
--- database.py
+++ database.py
@@ -2,7 +2,6 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
 user = os.environ['POSTGRES_USER']
 pwd = os.environ['POSTGRES_PASSWORD']
 db = os.environ['POSTGRES_DB']
@@ -11,9 +10,8 @@ port = '5432'
 engine = create_engine('postgres://%s:%s@%s:%s/%s' %
                        (user, pwd, host, port, db))
 
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
+db_session = scoped_session(
+    sessionmaker(autocommit=False, autoflush=False, bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
 
